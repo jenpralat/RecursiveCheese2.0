b41b60b5604fef8857d0571797936db16b13a6dd
jepralat <jepralat@wpi.edu>
2017-03-28 09:28:17 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/jenna/chorecloud/MakeChore.java
@@ -0,0 +1,41 @@
+package com.example.jenna.chorecloud;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
+
+public class MakeChore extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_chore);
+    }
+
+    public void MakeChore(){
+
+        //Need to figure out switch
+
+        EditText name = (EditText) findViewById(R.id.enterName);
+        EditText point = (EditText) findViewById(R.id.enterPoint);
+        EditText time = (EditText) findViewById(R.id.enterTime);
+        EditText due = (EditText) findViewById(R.id.enterDate);
+        EditText description = (EditText) findViewById(R.id.enterDescription);
+
+        Boolean repeat =
+
+        String nameStr = name.getText().toString();
+        String pointStr = point.getText().toString();
+        String timeStr = time.getText().toString();
+        String dueStr = due.getText().toString();
+        String descriptionStr = description.getText().toString();
+
+        int pointInt = parseInt(pointStr);
+        Double timeDouble = parseDouble(timeStr);
+        Double dueDouble = parseDouble(dueStr);
+    }
+
+}
