a829ef4bdd820886fa4db208ddb7bf72ccc9509a
jepralat <jepralat@wpi.edu>
2017-03-31 10:40:14 -0400

    Updateding something

--- app/src/main/java/com/example/jenna/chorecloud/MakeChore.java
+++ app/src/main/java/com/example/jenna/chorecloud/MakeChore.java
@@ -2,6 +2,7 @@ package com.example.jenna.chorecloud;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Switch;
 
@@ -16,30 +17,31 @@ public class MakeChore extends AppCompatActivity {
         setContentView(R.layout.activity_make_chore);
     }
 
-    public void MakeChore(){
+    public void MakeChore(View view) {
+            EditText name = (EditText) findViewById(R.id.enterName);
+            EditText point = (EditText) findViewById(R.id.enterPoint);
+            EditText time = (EditText) findViewById(R.id.enterTime);
+            EditText due = (EditText) findViewById(R.id.enterDate);
+            EditText description = (EditText) findViewById(R.id.enterDescription);
 
-        //Need to figure out switch
+            Switch repeat = (Switch) findViewById(R.id.enterRepeat);
+            Boolean repeatB = repeat.isChecked();
 
-        EditText name = (EditText) findViewById(R.id.enterName);
-        EditText point = (EditText) findViewById(R.id.enterPoint);
-        EditText time = (EditText) findViewById(R.id.enterTime);
-        EditText due = (EditText) findViewById(R.id.enterDate);
-        EditText description = (EditText) findViewById(R.id.enterDescription);
+            String nameStr = name.getText().toString();
+            String pointStr = point.getText().toString();
+            String timeStr = time.getText().toString();
+            String dueStr = due.getText().toString();
+            String descriptionStr = description.getText().toString();
 
-        Switch repeat = (Switch) findViewById(R.id.enterRepeat);
-        Boolean repeatB = repeat.isChecked();
+            int pointInt = parseInt(pointStr);
+            Double timeDouble = parseDouble(timeStr);
+            Double dueDouble = parseDouble(dueStr);
 
-        String nameStr = name.getText().toString();
-        String pointStr = point.getText().toString();
-        String timeStr = time.getText().toString();
-        String dueStr = due.getText().toString();
-        String descriptionStr = description.getText().toString();
-
-        int pointInt = parseInt(pointStr);
-        Double timeDouble = parseDouble(timeStr);
-        Double dueDouble = parseDouble(dueStr);
-
-        Chore chore = new Chore(pointInt, nameStr, timeDouble, descriptionStr, dueDouble, repeatB);
+            Chore chore = new Chore(pointInt, nameStr, timeDouble, descriptionStr, dueDouble, repeatB);
     }
 
-}
+    public void toChoreDisplay(View view) {
+
+    }
+
+
