58a6d1942005915601d911f4ca996e5309f19d39
Rithika Senthilkumar <rsenthilkumar@wpi.edu>
2017-04-03 14:46:10 -0400

    New activity - child view of chores

--- app/src/main/java/com/example/jenna/chorecloud/ChoreDisplay.java
+++ app/src/main/java/com/example/jenna/chorecloud/ChoreDisplay.java
@@ -9,10 +9,4 @@ import android.widget.TextView;
 
 public class ChoreDisplay extends AppCompatActivity {
 
-    TextView name = (TextView)findViewById(R.id.ChoreName);
-    TextView point = (TextView)findViewById(R.id.PointValue);
-    TextView time = (TextView)findViewById(R.id.Time);
-    TextView repeat = (TextView)findViewById(R.id.Repeat);
-    TextView duedate = (TextView)findViewById(R.id.DueDate);
-    TextView description = (TextView)findViewById(R.id.Description);
 }
