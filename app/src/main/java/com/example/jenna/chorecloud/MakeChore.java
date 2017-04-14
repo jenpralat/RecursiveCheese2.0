7c0500e92094cc965a1168b5fc6a761880672cf9
Jonathan Sigel <jonsigel@gmail.com>
2017-04-14 07:53:32 -0400

    Merge branch 'master' of C:\Users\Jonathan Sigel\AndroidStudioProjects\AppsForGood_JSS with conflicts.

--- app/src/main/java/com/example/jenna/chorecloud/MakeChore.java
+++ app/src/main/java/com/example/jenna/chorecloud/MakeChore.java
@@ -2,27 +2,31 @@ package com.example.jenna.chorecloud;
 
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
-import android.support.v4.app.NotificationCompat;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Switch;
-
+import android.content.Context;
+import android.support.v4.app.NotificationCompat;
 import static java.lang.Double.parseDouble;
 import static java.lang.Integer.parseInt;
-
+import com.firebase.client.Firebase;
 public class MakeChore extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_chore);
+
     }
 
     public void MakeChore (View view){
+
+        //FirebaseDatabase database = FirebaseDatabase.getInstance();
+
         EditText name = (EditText) findViewById(R.id.enterName);
         EditText point = (EditText) findViewById(R.id.enterPoint);
         EditText time = (EditText) findViewById(R.id.enterTime);
@@ -49,38 +53,26 @@ public class MakeChore extends AppCompatActivity {
 
     public void newIntent(View view, Chore chore){
         Intent intent = new Intent(this, ChoreDisplay.class);
-        String nameDisplay = "Wash the Dishes";//YO FUTURE JENNA YOU CHANGED THIS FOR YOUR TESTING, CHANGE IT BACK!!!!!!!!!
+        String nameDisplay = chore.getName();
         int pointDisplay = chore.getPoints();
+        Double timeDisplay = chore.getTime();
+        String descriptionDisplay = chore.getDesc();
         intent.putExtra("ChoreNameDisplay",nameDisplay);
         intent.putExtra("ChorePointDisplay",pointDisplay);
         startActivity(intent);
     }
+
     public void SendNotification(View view){
-
-        // Creates an Intent for the Activity
-        Intent intent = new Intent(this, ChoreDisplay.class);
-
-        // Sets the Activity to start in a new, empty task
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-
-        // Creates the PendingIntent
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        // Creates a Builder object.
+        //Get an instance of Notification Manager
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
-        mBuilder.setContentTitle("ChoreCloud");
-        mBuilder.setContentText("A new chore was created!");
+        mBuilder.setContentTitle("Chore Notification!");
+        mBuilder.setContentText("Please Work");
         mBuilder.setSmallIcon(android.R.drawable.ic_menu_report_image);
-        // Puts the PendingIntent into the notification builder
-        mBuilder.setContentIntent(pendingIntent);
-
-        // Notifications are issued by sending them to the NotificationManager system service
+        // Gets an instance of the NotificationManager service//
+        PendingIntent pi = PendingIntent.getActivity(this, 0, new Intent(this, ChoreDisplay.class), 0);
+        Resources r = getResources();
         NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        // Passes the Notification object to the NotificationManager.
-        mNotificationManager.notify(1, mBuilder.build());
+        mNotificationManager.notify(0, mBuilder.build());
     }
 
 }
-
-
