ddd5a538193cffab2a8dda2df42167d59d025a12
Rithika Senthilkumar <rsenthilkumar@wpi.edu>
2017-05-03 14:19:40 -0400

    (Firebase Authentication - Google Sign In)
    The door hissed shut behind Samuel as he stepped into the elvator. He could feel some sense of finality weighing on his shoulders as the elevator started its ascent, but couldn't help but hum to the inane music playing over the speakers. It was an odd comfort in a time where he most certainly needed it.

--- app/src/main/java/com/example/jenna/chorecloud/MakeChore.java
+++ app/src/main/java/com/example/jenna/chorecloud/MakeChore.java
@@ -3,7 +3,6 @@ package com.example.jenna.chorecloud;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
