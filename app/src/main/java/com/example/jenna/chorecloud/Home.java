558a6c1630d957bce02438fb0b5ecbfacc9de94d
Jonathan Sigel <jonsigel@gmail.com>
2017-05-09 13:26:54 -0400

    (Added a homescreen!)
    Samuel wasn't sure how many hours he spent in the elevator. Seconds to minutes, minutes to hours... He traced his fingers over the walls, feeling the doors, the buttons, all of them useless to him now.

---
+++ app/src/main/java/com/example/jenna/chorecloud/Home.java
@@ -0,0 +1,23 @@
+package com.example.jenna.chorecloud;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by Jonathan Sigel on 5/8/2017.
+ */
+
+public class Home extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homescreen);
+    }
+    public void onClick(View view){
+        Intent i = new Intent(this, NameListChoreDisplay.class);
+        startActivity(i);
+    }
+}
