0c00ddcea49039e5b1eafbdc12dee4a591966d9c
Jonathan Sigel <jonsigel@gmail.com>
2017-05-11 09:03:17 -0400

    (Pushing for a miracle)
    All of a sudden, the elevator doors began to shudder. Something was causing them to grind against the pistons sealing it shut... The doors slowly started to split open, inhumanly long fingers visible in the breach. They wrap around the elevator, trembling with effort as they pried the door open.

---
+++ app/src/main/java/com/example/jenna/chorecloud/Homescreen.java
@@ -0,0 +1,14 @@
+package com.example.jenna.chorecloud;
+
+import android.os.Bundle;
+
+/**
+ * Created by Jonathan Sigel on 5/8/2017.
+ */
+
+//public class Homescreen {
+    //protected void onCreate(Bundle savedInstanceState) {
+       // super.onCreate(savedInstanceState);
+       // setContentView(R.layout.homescreen);
+    //}
+//}
