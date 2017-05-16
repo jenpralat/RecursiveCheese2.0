eb1d0a7550b8bb21eb586fcb4ab1d17f230db170
Jonathan Sigel <jonsigel@gmail.com>
2017-05-16 15:22:18 -0400

    Start Activity

---
+++ app/src/main/java/com/example/jenna/chorecloud/chooseone.java
@@ -0,0 +1,27 @@
+/**
+ * Created by Jonathan Sigel on 5/16/2017.
+ */
+
+package com.example.jenna.chorecloud;
+
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.view.View;
+
+/**
+ * Created by rithi on 5/16/2017.
+ */
+public class chooseone extends AppCompatActivity {
+
+    //Display the new chore
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.choose_one);
+    }
+    public void onClick(View view){
+
+        Intent a = new Intent(this, NameListChoreDisplay.class);
+        startActivity(a);
+    }
+}
